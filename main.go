1be431733b1f48e8f5ec7eec4896602de1feb7e8
Rodrigo <rofen1@protonmail.com>
2025-11-04 16:51:41 -0300

    feat: Criar router

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rodrigofnobrega/gopportunities/router"
+
+func main() {
+	router.InitializeRoutes()
+}
---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func InitializeRoutes() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":8080")
+}
