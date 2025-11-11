7339968e396603b411f84354b0a2bc7df52f343f
Rodrigo <rofen1@protonmail.com>
2025-11-11 16:41:52 -0300

    feat: Criar  logger e schemas

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/rodrigofnobrega/gopportunities/router"
+import (
+	"github.com/rodrigofnobrega/gopportunities/config"
+	"github.com/rodrigofnobrega/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errf("config initialization erro: %v", err)
+		return
+	}
+
 	router.InitializeRoutes()
 }
